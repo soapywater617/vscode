81e7743cbab0ddeb681c7a07b5e2d73636047424
Sophie Yu <115031039+soapywater617@users.noreply.github.com>
2025-08-24 21:51:22 -0700

    Update app.py
    change first img placeholder to a real placeholder

--- gg_bakery/app.py
+++ gg_bakery/app.py
@@ -186,7 +186,7 @@ if page == "Landing Page":
         with col2:
             st.image(logo_path, use_container_width=False, width=220)
     else:
-        st.image("assets/randompng.png", use_container_width=False, width=220)
+        st.image("https://placehold.co/160x160?text=Logo", use_container_width=False, width=220)
     st.write("")
 
     # Recent Desserts
