3e345e5402621b94e92f05f4ebd2d62ace6625e7
Sophie Yu <115031039+soapywater617@users.noreply.github.com>
2025-08-24 21:24:48 -0700

    Update app.py

--- gg_bakery/app.py
+++ gg_bakery/app.py
@@ -186,7 +186,7 @@ if page == "Landing Page":
         with col2:
             st.image(logo_path, use_container_width=False, width=220)
     else:
-        st.image("assets/tira1.png", use_container_width=False, width=220)
+        st.image("assets/tira.png", use_container_width=False, width=220)
     st.write("")
 
     # Recent Desserts
