2f37ff17098f9d04c885986a6ac4a92d4e558b26
Sophie Yu <115031039+soapywater617@users.noreply.github.com>
2025-08-26 11:20:18 -0700

    Update app.py
    missing quote

--- gg_bakery/app.py
+++ gg_bakery/app.py
@@ -398,7 +398,7 @@ elif page == "Events & Sales":
                 if img is not None:
                     st.image(img, use_container_width=True)
                 
-                st.markdown('<div class="greybody>'texts[i+j]'</div>')
+                st.markdown('<div class="greybody">'texts[i+j]'</div>')
                 st.markdown('<div style="font-family: Gaegu, cursive; font-size: 16px; color: #222; font-weight: bold;">Product</div>', unsafe_allow_html=True)
                 st.markdown('<div class="greybody">Description of first product</div>', unsafe_allow_html=True)
                 st.markdown('<div class="greybody" style="margin-bottom: 1.5em">$10.99</div>', unsafe_allow_html=True)
