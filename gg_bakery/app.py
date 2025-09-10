263e69ffe085c9df52e20a36669347d1f427a468
Sophie Yu <sophie.20.yu@gmail.com>
2025-09-09 23:18:43 -0700

    update day

--- gg_bakery/app.py
+++ gg_bakery/app.py
@@ -354,8 +354,8 @@ if page == "Landing Page":
                     <li>6</li>
                     <li>7</li>
                     <li>8</li>
-                    <li><span class="active">9</span></li>
-                    <li>10</li>
+                    <li>9</li>
+                    <li><span class="active">10</span></li>
                     <li>11</li>
                     <li>12</li>
                     <li>13</li>
